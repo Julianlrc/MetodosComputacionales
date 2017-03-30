b8ba856240e0135ea2345445c9520e52daa27c16
JulianRodriguez <jl.rodriguez11@uniandes.edu.co>
2017-03-30 15:13:53 -0500

    Taller 1 - 2017-10

---
+++ Taller_1/CurvasDeRotacion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir RotationCurves
+
+wget http://iopscience.iop.org/1538-3881/122/5/2396/fulltext/datafile3.txt
+
+awk '{ if($1=="F571-8") print $2,$3,$4,$5,$6,$7,$8;}' datafile3.txt > RotationCurve_F571-8.txt
+
+rm datafile3.txt
+mv PLOTS_RotationCurves.py RotationCurves
+mv RotationCurve_F571-8.txt RotationCurves
+cd RotationCurves
+
+python PLOTS_RotationCurves.py
