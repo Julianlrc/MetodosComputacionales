c7467afd940bc6ba8e559e62b7030c89a1fd9774
Julian Rodriguez <jl.rodriguez11@uniandes.edu.co>
2017-04-29 03:27:15 -0500

    Lo importante es que hay salud. Caso 1 y 2 de las fijas (salen mal, creo)

--- Taller_4/DifusionTemperatura.c
+++ Taller_4/DifusionTemperatura.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-float solver(float nu, float alpha, float T[100][100]);
-void inicializarCaso1(float temp_rect, float temp_placa, float T[100][100]);
-void condicionesFijas(float t_fija, float T[100][100]);
+float solverUno(float nu, float alpha, float T[100][100]);
+void inicializar(float temp_rect, float temp_placa, float T[100][100]);
+float solverDos(float nu, float alpha, float T[100][100]);
 
 int main()
 {
-	float nu = 0.1;
+	float nu = 0.0001;
 	float dx = 1.0;
-	float dt = 0.1;
-	float alpha = dt/(dx*dx);
+	float dt = 0.2;
+	float alpha = dt/(dx*dx);	
+
+	// CASO 1 // 
+
+	// CONDICIONES DE FRONTERA FIJAS //	
+
+	int i,j;
 
 	float T_f_2500[100][100];
-	inicializarCaso1(100.0, 50.0, T_f_2500);
-	
-	int i,j;
+       	inicializar(100.0, 50.0, T_f_2500);
+       
 	FILE *fijas_0 = fopen("fijas_caso1_0.txt", "w+");	
 	FILE *fijas_2500 = fopen("fijas_caso1_2500.txt", "w+");
 	FILE *fijas_100 = fopen("fijas_caso1_100.txt", "w+");
@@ -28,23 +33,23 @@ int main()
 			fprintf(fijas_0, "%f \n", T_f_2500[i][j]);
 		}
 	}
+	fclose(fijas_0);
 
 	float T_f_100[100][100];
-	inicializarCaso1(100.0, 50.0, T_f_100);
-
+	inicializar(100.0, 50.0, T_f_100);
+	
 	for(i=0; i<(2500/dt);i++)
 	{	
-		float matrix[100][100];
-		matrix[100][100] = solver(nu, alpha, T_f_2500);
+		solverUno(nu, alpha, T_f_2500);
 		
 		if(i==(100/dt-1))
 		{	
-			T_f_100[100][100]= matrix[100][100];
+			T_f_100[100][100]= T_f_2500[100][100];
 		}
 
 		if(i==(2500/dt-1))
 		{
-			T_f_2500[100][100] = matrix[100][100];
+			T_f_2500[100][100] = T_f_2500[100][100];
 		}	
 	}
 
@@ -56,14 +61,64 @@ int main()
 			fprintf(fijas_100, "%f \n", T_f_100[i][j]);
 		}
 	}
+	fclose(fijas_100);
+	fclose(fijas_2500);
+
+	// CASO 2 //
+
+	// C.F FIJAS //
+
+	float T_f2_2500[100][100];
+       	inicializar(100.0, 50.0, T_f2_2500);
+       
+	FILE *fijas2_0 = fopen("fijas_caso2_0.txt", "w+");	
+	FILE *fijas2_2500 = fopen("fijas_caso2_2500.txt", "w+");
+	FILE *fijas2_100 = fopen("fijas_caso2_100.txt", "w+");
+	
+	for(i=0;i<100;i++)
+	{	
+		for(j=0;j<100;j++)
+		{
+			fprintf(fijas2_0, "%f \n", T_f2_2500[i][j]);
+		}
+	}
+	fclose(fijas2_0);
+
+	float T_f2_100[100][100];
+	inicializar(100.0, 50.0, T_f2_100);
+		
+	for(i=0; i<(2500/dt);i++)
+	{	
+		solverDos(nu, alpha, T_f2_2500);
+		
+		if(i==(100/dt-1))
+		{	
+			T_f2_100[100][100]= T_f2_2500[100][100];
+		}
+
+		if(i==(2500/dt-1))
+		{
+			T_f2_2500[100][100] = T_f2_2500[100][100];
+		}	
+	}
+
+	for(i=0; i<100; i++)
+	{
+		for(j=0; j<100; j++)
+		{
+			fprintf(fijas2_2500, "%f \n", T_f_2500[i][j]);
+			fprintf(fijas2_100, "%f \n", T_f_100[i][j]);
+		}
+	}
+	fclose(fijas2_100);
+	fclose(fijas2_2500);
 
 	return 0;
         
 }
 
-float solver(float nu, float alpha, float T[100][100])
+float solverUno(float nu, float alpha, float T[100][100])
 {	
-	//FILE *temp = fopen("temp.txt", "w+");
 	int i,j;
 
 	float T_past[100][100];
@@ -82,17 +137,14 @@ float solver(float nu, float alpha, float T[100][100])
 			T[i][j]= nu*alpha*(T_past[i+1][j]+T_past[i-1][j]+T_past[i][j+1]+T_past[i][j-1]-4*T_past[i][j])+T_past[i][j];
 		}
 		
-		//fprintf(temp, "%f \n", T[i][j]);
 	}
 
 	return T[100][100];
 }
 
-void inicializarCaso1(float temp_rect, float temp_placa, float T[100][100])
+void inicializar(float temp_rect, float temp_placa, float T[100][100])
 {
 	int i,j;
-
-	//FILE *out = fopen("iniciales_abiertas.txt", "w+");		
 	for(i=0;i<100;i++)
 	{
 		for(j=0;j<100;j++)
@@ -104,30 +156,35 @@ void inicializarCaso1(float temp_rect, float temp_placa, float T[100][100])
 			else
 			{
 				T[i][j] = temp_placa;
-			}
-			//fprintf(out, "%f \n", T[i][j]);		
+			}		
 		}
 	}
 
 }
 
-void condicionesFijas(float t_fija, float T[100][100])
+float solverDos(float nu, float alpha, float T[100][100])
 {
-	int i,j;
+	
+	int i,j;                                        
+        float T_past[100][100];
+        for(i=0;i<100;i++)
+        {
+        	for(j=0;j<100;j++)
+        	{
+        		T_past[i][j] = T[i][j];
+		}
+	}
 
-	for(i=0; i<100;i++)
-	{
-		for(j=0; j<100; j++)
-		{	
-			if(i==0 || i==99)
+	for(i=1; i<99; i++)
+       	{
+        	for(j=1; j<99; j++)
+       		{
+			if(!(i>=20 && i<40 && j>=45 && j<55))
 			{
-				T[i][j] = t_fija;
-			}
-			
-			if(i!=0 && j==0 || i!=0 && j==99)
-			{
-				T[i][j] = t_fija;
+	    			T[i][j]= nu*alpha*(T_past[i+1][j]+T_past[i-1][j]+T_past[i][j+1]+T_past[i][j-1]-4*T_past[i][j])+T_past[i][j];
 			}
 		}
 	}
+
+	return T[100][100];
 }
--- Taller_4/Plots_Temperatura.py
+++ Taller_4/Plots_Temperatura.py
@@ -2,14 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-fijas_1_2500 = np.loadtxt("fijas_caso1_2500.txt")
-fijas_1_0 = np.loadtxt("fijas_caso1_0.txt")
-fijas_1_100 = np.loadtxt("fijas_caso1_100.txt")
+fijas1_2500 = np.loadtxt("fijas_caso1_2500.txt")
+fijas1_0 = np.loadtxt("fijas_caso1_0.txt")
+fijas1_100 = np.loadtxt("fijas_caso1_100.txt")
+fijas2_2500 = np.loadtxt("fijas_caso2_2500.txt")
+fijas2_0 = np.loadtxt("fijas_caso2_0.txt")
+fijas2_100 = np.loadtxt("fijas_caso2_100.txt")
 
-
-t_inicial = np.zeros((100,100), dtype=float)
-t_post2500 = np.zeros((100,100), dtype=float)
-t_post100 = np.zeros((100,100), dtype=float)
+t_fijas1_0 = np.zeros((100,100), dtype=float)
+t_fijas1_100 = np.zeros((100,100), dtype=float)
+t_fijas1_2500 = np.zeros((100,100), dtype=float)
+t_fijas2_0 = np.zeros((100,100), dtype=float)
+t_fijas2_100 = np.zeros((100,100), dtype=float)
+t_fijas2_2500 = np.zeros((100,100), dtype=float)
 
 def linealizar(matriz, data):
 	k=0
@@ -18,18 +23,28 @@ def linealizar(matriz, data):
 			matriz[j][i]= data[k]
 			k+=1
 
-linealizar(t_inicial, fijas_1_0)
-linealizar(t_post2500, fijas_1_2500)
-linealizar(t_post100, fijas_1_100)
+linealizar(t_fijas1_0, fijas1_0)
+linealizar(t_fijas1_2500, fijas1_2500)
+linealizar(t_fijas1_100, fijas1_100)
+linealizar(t_fijas2_0, fijas2_0)
+linealizar(t_fijas2_2500, fijas2_2500)
+linealizar(t_fijas2_100, fijas2_100)
 	
-#f, (ax1, ax2) = plt.subplots(2)
-#plt.figure()
-plt.imshow(t_inicial)
+plt.imshow(t_fijas1_0)
 plt.colorbar()
 plt.show()
-plt.imshow(t_post100)
+plt.imshow(t_fijas1_100)
 plt.colorbar()
 plt.show()
-plt.imshow(t_post2500)
+plt.imshow(t_fijas1_2500)
+plt.colorbar()
+plt.show()
+plt.imshow(t_fijas2_0)
+plt.colorbar()
+plt.show()
+plt.imshow(t_fijas2_100)
+plt.colorbar()
+plt.show()
+plt.imshow(t_fijas2_2500)
 plt.colorbar()
 plt.show()
