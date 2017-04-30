c3cd0c9f7479d6a9c3b1870e72e7f94724311c7f
Julian Rodriguez <jl.rodriguez11@uniandes.edu.co>
2017-04-29 19:48:19 -0500

    Tex organizado

--- Taller_4/Tarea4.mk
+++ Taller_4/Tarea4.mk
@@ -1,13 +1,15 @@
 plots = f1_0.pdf f1_100.pdf f1_2500.pdf f2_0.pdf f2_100.pdf f2_2500.pdf a1_0.pdf a1_100.pdf a1_2500.pdf a2_0.pdf a2_100.pdf a2_2500.pdf p1_0.pdf p1_100.pdf p1_2500.pdf p2_0.pdf p2_100.pdf p2_2500.pdf prom1.pdf prom2.pdf 
 
-Resultados_hw4.pdf : Resultados_hw4.tex $(plots)
-	pdflatex $<
+files = fijas_caso1_0.txt fijas_caso1_100.txt fijas_caso1_2500.txt fijas_caso2_0.txt fijas_caso2_100.txt fijas_caso2_2500.txt abiertas_caso1_0.txt abiertas_caso1_100.txt abiertas_caso1_2500.txt abiertas_caso2_0.txt abiertas_caso2_100.txt abiertas_caso2_2500.txt periodicas_caso1_0.txt periodicas_caso1_100.txt periodicas_caso1_2500.txt periodicas_caso2_0.txt periodicas_caso2_100.txt periodicas_caso2_2500.txt fijas_caso1_prom.txt fijas_caso2_prom.txt abiertas_caso1_prom.txt abiertas_caso2_prom.txt periodicas_caso1_prom.txt periodicas_caso2_prom.txt
 
-$(plots) : Plots_Temperatura.py
-	python $<
+Resultados_hw4.pdf : $(plots) Resultados_hw4.tex
+	pdflatex Resultados_hw4.tex
 
-*.txt : a.out
-	./$<
+$(plots) : Plots_Temperatura.py $(files)
+	python Plots_Temperatura.py
+
+$(files) : a.out
+	./a.out
 
 a.out : DifusionTemperatura.c
-	cc $<
+	cc -lm DifusionTemperatura.c
