00c35ce95a6062664e947d3b04f28f8637ca680c
Julian Rodriguez <jl.rodriguez11@uniandes.edu.co>
2017-05-24 14:02:19 -0500

    Listo el pollo

--- Taller_5/canal_ionico.c
+++ Taller_5/canal_ionico.c
@@ -9,7 +9,7 @@ void guardarEnListas(int nfilas, FILE *in, float *x, float *y);
 float max(int len, float *arreglo);
 float min(int len, float *arreglo);
 float calcularRadio(float x1, float x2, float y1, float y2);
-void mcmc(float *x, float *y, char nombreFile[]);
+void mcmc(int length, float *x, float *y, char nombreFile[]);
 
 int main()
 {
@@ -21,7 +21,7 @@ int main()
 	float *x = malloc((n/2)*sizeof(float));
 	float *y = malloc((n/2)*sizeof(float));
 	guardarEnListas(n, in, x , y);
-	mcmc(x, y, "walk.txt");
+	mcmc(n/2, x, y, "walk.txt");
 
 	// Se hace MCMC para el archivo Canal_ionico1.txt
 	FILE *in1 = fopen("Canal_ionico1.txt", "r");
@@ -30,7 +30,7 @@ int main()
 	float *x1 = malloc((n1/2)*sizeof(float));
 	float *y1 = malloc((n1/2)*sizeof(float));
 	guardarEnListas(n1, in1, x1, y1);
-	mcmc(x1, y1, "walk1.txt");
+	mcmc(n1/2, x1, y1, "walk1.txt");
 
 	return 0;	
 }
@@ -80,7 +80,7 @@ float calcularRadio(float x1, float x2, float y1, float y2)
 	float y = y2-y1;	
 	float resultado = sqrt(x*x+y*y)-r_mol;	
 
-	if(resultado<0.0)
+	if(resultado<1.0)
 	{
 		resultado=0.0;
 	}	
@@ -120,28 +120,29 @@ float min(int len, float *arreglo)
 	return min;
 }
 
-void mcmc(float *x, float *y, char nombreFile[])
+void mcmc(int length, float *x, float *y, char nombreFile[])
 {
 	float alpha, beta;
 	int i,j;
 	srand(time(NULL));
-	int it = 200000;
+	int it = 300000;
 	float *x_walk = malloc(it*sizeof(float));
 	float *y_walk = malloc(it*sizeof(float));
 	float *r_walk = malloc(it*sizeof(float));
 
-	float x_max = max(sizeof(x), x);
-	float x_min = min(sizeof(x), x);
-	float y_max = max(sizeof(y), y);
-	float y_min = min(sizeof(y), y);
+	float x_max = max(length, x);
+	float x_min = min(length, x);
+	float y_max = max(length, y);
+	float y_min = min(length, y);
 	
 	x_walk[0] = rand()*(x_max-x_min)/RAND_MAX + x_min;
 	y_walk[0] = rand()*(y_max-y_min)/RAND_MAX + y_min;
-	r_walk[0] = calcularRadio(x[0], x_walk[0], y[0], y_walk[0]);
-	
-	for(i=1; i<sizeof(x); i++)
+	r_walk[0] = calcularRadio(x_walk[0], x[0], y_walk[0], y[0]);
+	float r;	
+
+	for(i=1; i<length; i++)
 	{
-		float r = calcularRadio(x[i], x_walk[0], y[i], y_walk[0]);
+		r = calcularRadio(x_walk[0], x[i], y_walk[0], y[i]);
 		if(r<r_walk[0])
 		{
 			r_walk[0]=r;
@@ -151,62 +152,68 @@ void mcmc(float *x, float *y, char nombreFile[])
 	FILE *out = fopen(nombreFile, "w+");
 	fprintf(out, "%f %f %f \n", x_walk[0], y_walk[0], r_walk[0]);
 
+	float x_prime = x_min;
+	float y_prime = y_min;
+	float r_prime;
+	int n=0;
 	for(i=0; i<it; i++)
 	{
-		float x_prime;
-		float y_prime;
 
 		float U = (float)rand()/(float)RAND_MAX;
 		float V = (float)rand()/(float)RAND_MAX;
 		float X = sqrt(-2*log(U))*cos(2*M_PI*V);
 		float Y = sqrt(-2*log(U))*sin(2*M_PI*V);
-		x_prime = 0.1*X + x_walk[i];
-		y_prime = 0.1*Y + y_walk[i];
-
-		float r_prime;
+		x_prime = (1.0*X + x_walk[i]);
+		y_prime = (1.0*Y + y_walk[i]);
 
 		if(x_prime > x_min && x_prime < x_max && y_prime > y_min && y_prime < y_max)			
 		{	
-			r_prime = calcularRadio(x[0], x_prime, y[0], y_prime);
-			for(j=1; j<sizeof(x); j++)
-			{
-				float rr = calcularRadio(x[j], x_prime, y[j], y_prime);
-				if(rr < r_prime)
-				{
-					r_prime = rr;
-				}	
-			}	
-		}
-		
-		alpha = r_prime/r_walk[i];
+			r_prime = calcularRadio(x_prime, x[0], y_prime, y[0]);
+			for(j=1; j<length; j++)
+			{	
+				r = calcularRadio(x_prime, x[j], y_prime, y[j]);
+				if(r < r_prime)
+				{					
+					r_prime = r;
+				}			
+			}
 
-		if(alpha>=1.0)
+			alpha = r_prime/r_walk[i];                                                                                            
+                	if(alpha>=1.0)
+                	{
+                		x_walk[i+1]=x_prime;
+                		y_walk[i+1]=y_prime;
+                		r_walk[i+1]=r_prime;
+                	}
+                        else
+                        {
+                   		beta = rand()%2;
+                		if(beta<=alpha)
+                		{		
+                			x_walk[i+1]=x_prime;
+                			y_walk[i+1]=y_prime;
+                			r_walk[i+1]=r_prime;
+                		}
+                		else
+                		{
+                			x_walk[i+1]=x_walk[i];
+                			y_walk[i+1]=y_walk[i];
+                			r_walk[i+1]=r_walk[i];
+                		}
+                		
+                	}
+                	
+                	fprintf(out, "%f %f %f \n", x_walk[i+1], y_walk[i+1], r_walk[i+1]);	
+		}
+		
+		else
 		{
-			x_walk[i+1]=x_prime;
-			y_walk[i+1]=y_prime;
-			r_walk[i+1]=r_prime;
+			x_walk[i+1]=x_walk[i];
+			y_walk[i+1]=y_walk[i];
+			r_walk[i+1]=r_walk[i];
+			fprintf(out, "%f %f %f \n", x_walk[i+1], y_walk[i+1], r_walk[i+1]);
 		}
-		
-                else
-                {
-           		beta = rand()%2;
-			if(beta<=alpha)
-			{		
-				x_walk[i+1]=x_prime;
-				y_walk[i+1]=y_prime;
-				r_walk[i+1]=r_prime;
-			}
-			else
-			{
-				x_walk[i+1]=x_walk[i];
-				y_walk[i+1]=y_walk[i];
-				r_walk[i+1]=r_walk[i];
-			}
-			
-		}
-		
-		fprintf(out, "%f %f %f \n", x_walk[i+1], y_walk[i+1], r_walk[i+1]);
 	}
 
-	fclose(out);	
+		fclose(out);	
 }
